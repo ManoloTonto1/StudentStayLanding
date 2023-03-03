6d0cdfa2ec40a2860e1650676a01b28f62cefa66
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-03-03 21:12:52 +0100

    init

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { Canvas, useFrame } from '@react-three/fiber'
+import React, { useRef, useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App/>)
