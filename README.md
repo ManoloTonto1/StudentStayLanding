0112f18d702252e377d0ef004b35a232e6153224
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-03-04 13:05:06 +0100

    finished

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🤫
+### Project coming soon...
+- [Niki Somogyi](https://github.com/pusziNiki)
+- [Manuel Lopez](https://github.com/ManoloTonto1)
+
--- main.js
+++ main.js
@@ -1,89 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-let scene, camera, renderer;
-let geoArr = [];
-let starGeo;
-let stars;
-let width = window.innerWidth;
-let height = window.innerHeight;
-
-addEventListener("resize", (event) => {
-  width = window.innerWidth;
-  height = window.innerHeight;
-});
-
-onresize = (event) => {
-  width = window.innerWidth;
-  height = window.innerHeight;
-};
-
-
-function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    75,
-    width / height,
-    1,
-    1000);
-  camera.rotation.x = Math.PI * 0.5;
-
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(width, height);
-  document.body.appendChild(renderer.domElement);
-
-  for (let i = 0; i < 10000; i++) {
-    const star = new THREE.Vector3(
-      Math.random() * 2000 - 1600,
-      Math.random() * 2000,
-      Math.random() * 2000 - 1600,
-    );
-    star.velocity = 0;
-    star.acceleration = 0.001;
-    geoArr.push(star);
-
-
-  };
-  starGeo = new THREE.BufferGeometry().setFromPoints(geoArr);
-
-  let sprite = new THREE.TextureLoader().load("circle.png");
-  let starMaterial = new THREE.PointsMaterial({
-    color: "gray",
-    size: 0.6,
-    map: sprite,
-  });
-  stars = new THREE.Points(starGeo, starMaterial);
-  scene.add(stars);
-  const loader = new GLTFLoader();
-  loader.load(
-    'studentstay.gltf',
-    (asset) => {
-
-      asset.scene.scale.set(0.2, 0.2, 0.2);
-      asset.scene.position.set(0, 1.5, 0);
-      asset.scene.rotation.x = Math.PI;
-      scene.add(asset.scene);
-    },
-    (xhr) => {
-      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    (error) => {
-      console.log('An error happened');
-    }
-  );
-
-  const light2 = new THREE.PointLight(0xffffff, 0.8);
-  light2.position.set(-2.5, -2, 0);
-  scene.add(light2);
-  const light3 = new THREE.PointLight(0xffffff, 0.8);
-  light3.position.set(3, -2, 0);
-  scene.add(light3);
-
-
-
-  animate();
-}
 function animate() {
   const positions = starGeo.attributes.position.array;
   for (let i = 0; i < geoArr.length; i++) {
@@ -98,9 +16,115 @@ function animate() {
 
   starGeo.attributes.position.needsUpdate = true;
   stars.rotation.y += 0.002
-  renderer.render(scene, camera);
   requestAnimationFrame(animate);
+  controls.update()
+  renderer.render(scene, camera);
+
 }
 
-init();
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function generateStars() {
+  for (let i = 0; i < 10000; i++) {
+    const star = new THREE.Vector3(
+      Math.random() * 2000 - 1600,
+      Math.random() * 2000,
+      Math.random() * 2000 - 1600
+    );
+    star.velocity = 0;
+    star.acceleration = 0.001;
+    geoArr.push(star);
+  };
+}
+window.addEventListener('resize', onWindowResize, false);
+
+let geoArr = [];
+let starGeo;
+let stars;
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  100,
+  width / height,
+  .01,
+  1000);
+
+// camera.rotation.x = Math.PI * 0.5;
+camera.position.y = Math.PI * -0.5+1
+camera.position.x = 0;
+const renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+renderer.setSize(width, height);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.01;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 1;
+
+generateStars();
+starGeo = new THREE.BufferGeometry().setFromPoints(geoArr);
+
+const sprite = new THREE.TextureLoader().load("Ellipse.png");
+
+const starMaterial = new THREE.PointsMaterial({
+  color: 0x39c58a,
+  size: 1.2,
+  map: sprite,
+});
+stars = new THREE.Points(starGeo, starMaterial);
+scene.add(stars);
+
+const loader = new GLTFLoader();
+loader.load(
+  'studentstay.gltf',
+  (asset) => {
+
+    asset.scene.scale.set(0.2, 0.2, 0.2);
+    asset.scene.position.set(0, 1.5, 0);
+    asset.scene.rotation.x = Math.PI;
+    scene.add(asset.scene);
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  (error) => {
+    console.log('An error happened');
+  }
+);
+
+
+const light2 = new THREE.PointLight(0xffffff, 0.8);
+light2.position.set(-2.5, -2, 0);
+scene.add(light2);
+const light3 = new THREE.PointLight(0xffffff, 0.8);
+light3.position.set(3, -2, 0);
+scene.add(light3);
+const pictureTexture = new THREE.TextureLoader().load('niki and me.jpg');
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 0, 3),
+  new THREE.MeshBasicMaterial({
+    map: pictureTexture,
+  })
+);
+cube.position.set(0, -10, 0);
+scene.add(cube);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper);
+
+
+animate();
+
+
+
+
 
